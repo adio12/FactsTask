2d26eaf44d462c6767af8027d145b2016c433a39
Aditya Yadav <adi.jss012@gmail.com>
2018-07-02 12:34:44 +0530

    Initial view displayed using code (Storyboard removed).

--- FactsTask/FactsTask/AppDelegate.swift
+++ FactsTask/FactsTask/AppDelegate.swift
@@ -16,6 +16,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        
+        let nav = UINavigationController()
+        nav.isNavigationBarHidden = true
+        
+        let factsVC = FactsViewController()
+        nav.viewControllers = [factsVC]
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+
         return true
     }
 
---
+++ FactsTask/FactsTask/ViewControllers/FactsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FactsViewController.swift
+//  FactsTask
+//
+//  Created by Aditya Yadav on 02/07/18.
+//  Copyright © 2018 Aditya Yadav. All rights reserved.
+//
+
+import UIKit
+
+class FactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.yellow
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
